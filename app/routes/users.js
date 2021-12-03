cc20f7ea642b5c705e57dfb47c4aca609f6d49d1
Muhammad Kashif <muhammad.kashif@probaseapps.com>
2021-12-03 15:56:55 +0500

    added user list data from api

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
 
-export default class UsersRoute extends Route {}
+export default class UsersRoute extends Route {
+  async model() {
+    const response = await fetch('https://gorest.co.in/public/v1/users');
+    const result = await response.json();
+    const users = result.data;
+    return users;
+  }
+}
