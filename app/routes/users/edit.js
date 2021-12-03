5236ef4d5c344e2bab818e4029909b128680bad4
Muhammad Kashif <muhammad.kashif@probaseapps.com>
2021-12-03 19:00:35 +0500

    user edit form

--- app/routes/users/edit.js
+++ app/routes/users/edit.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
 
-export default class UsersEditRoute extends Route {}
+export default class UsersEditRoute extends Route {
+  async model(params) {
+    let response = await fetch(
+      'https://gorest.co.in/public/v1/users/' + params.userid
+    );
+    let result = await response.json();
+    let user = result.data;
+    return user;
+  }
+}
