5c3b5575dd25ad11d5e880807dba4c723f5859b0
Muhammad Kashif <muhammad.kashif@probaseapps.com>
2021-12-03 18:43:57 +0500

    added get methods for all routes

--- app/routes/todo/index.js
+++ app/routes/todo/index.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
 
-export default class TodoIndexRoute extends Route {}
+export default class TodoIndexRoute extends Route {
+  async model() {
+    let response = await fetch('https://gorest.co.in/public/v1/todos');
+    let result = await response.json();
+    let todos = result.data;
+    return todos;
+  }
+}
