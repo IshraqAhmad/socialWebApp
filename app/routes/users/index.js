3ac0c1bdb568727f91f3d49e15ffca8237af003c
Muhammad Kashif <muhammad.kashif@probaseapps.com>
2021-12-06 12:55:40 +0500

    added pagination component

--- app/routes/users/index.js
+++ app/routes/users/index.js
@@ -1,10 +1,18 @@
 import Route from '@ember/routing/route';
 
 export default class UsersIndexRoute extends Route {
-  async model() {
-    const response = await fetch('https://gorest.co.in/public/v1/users');
+  queryParams = {
+    page: {
+      refreshModel: true,
+      replace: true,
+    },
+  };
+
+  async model(params) {
+    const response = await fetch(
+      'https://gorest.co.in/public/v1/users?page=' + params.page
+    );
     const result = await response.json();
-    const users = result.data;
-    return users;
+    return result;
   }
 }
