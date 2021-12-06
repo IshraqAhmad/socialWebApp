927cf8180cf64d141e3d0ec3f70733c1d7555734
Muhammad Kashif <muhammad.kashif@probaseapps.com>
2021-12-06 14:30:59 +0500

    added more componets

--- tests/integration/components/posts-test.js
+++ tests/integration/components/posts-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | user-todos', function (hooks) {
+module('Integration | Component | posts', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<UserTodos />`);
+    await render(hbs`<Posts />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <UserTodos>
+      <Posts>
         template block text
-      </UserTodos>
+      </Posts>
     `);
 
     assert.dom(this.element).hasText('template block text');
--- tests/integration/components/todos-test.js
+++ tests/integration/components/todos-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | user-posts', function (hooks) {
+module('Integration | Component | todos', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<UserPosts />`);
+    await render(hbs`<Todos />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <UserPosts>
+      <Todos>
         template block text
-      </UserPosts>
+      </Todos>
     `);
 
     assert.dom(this.element).hasText('template block text');
