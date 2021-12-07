292b18efc3d15a78a1d07a25da21a718d92b9fb6
Asad Wali <asadhamid27@gmail.com>
2021-12-07 15:14:38 +0500

    forms

--- tests/integration/components/users/user-form-test.js
+++ tests/integration/components/users/user-form-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | users/form', function (hooks) {
+module('Integration | Component | users/user-form', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Users::Form />`);
+    await render(hbs`<Users::UserForm />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <Users::Form>
+      <Users::UserForm>
         template block text
-      </Users::Form>
+      </Users::UserForm>
     `);
 
     assert.dom(this.element).hasText('template block text');
