2fd98e998415863e0e11b57d23740baf8da054b6
Asad Wali <asadhamid27@gmail.com>
2021-12-08 14:03:06 +0500

    Add Todos

--- app/controllers/users/view.js
+++ app/controllers/users/view.js
@@ -4,9 +4,14 @@ import { tracked } from '@glimmer/tracking';
 
 export default class UsersViewController extends Controller {
   @tracked post = null;
+  @tracked todo = null;
 
   @action
   setPost(post) {
     this.post = post;
+  }
+  @action
+  setTodo(todo) {
+    this.todo = todo;
   }
 }
